628b165739e98d1d99fd49d9c6c25570545ecc86
Sarah <Sarah@DESKTOP-IF9KM40>
2022-02-19 19:33:21 +0000

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SMS.Web.Models;
+
+namespace SMS.Web.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.LongTime = DateTime.Now.ToLongTimeString();
+        ViewBag.Message = "Time Now";
+
+        return View();
+    }
+    public IActionResult About()
+    {
+        
+        return View(new AboutViewModel { 
+            Formed=new DateTime(2022,1,1),
+            Message="This About Page was made by Sarah", 
+            Title ="About Page"
+
+        });
+
+
+        
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/AboutViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SMS.Web.Models
+{
+    public class AboutViewModel
+    {
+
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public DateTime Formed { get; set; } = DateTime.Now;
+        public string FormedString => Formed.ToLongDateString();
+        public int Days => (DateTime.Now - Formed).Days;
+    }
+
+}
+
